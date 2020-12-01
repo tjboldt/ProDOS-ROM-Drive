5f42dcb8a0509b709f2cf4c16e6a6e6f4e07594d
Terence Boldt <terence@boldt.ca>
2020-11-30 21:38:13 -0500

    Update assembly instructions

--- Firmware/Firmware.asm
+++ Firmware/Firmware.asm
@@ -5,6 +5,7 @@
 
 ;Install cc65 (on Ubuntu this is: sudo apt install cc65)
 ;Execute the following commands to generate the binary:
+;ca65 Warning.asm -o warning.o
 ;ca65 Firmware.asm -D SLOT1 -o slot1.o
 ;ca65 Firmware.asm -D SLOT2 -o slot2.o
 ;ca65 Firmware.asm -D SLOT3 -o slot3.o
@@ -12,7 +13,7 @@
 ;ca65 Firmware.asm -D SLOT5 -o slot5.o
 ;ca65 Firmware.asm -D SLOT6 -o slot6.o
 ;ca65 Firmware.asm -D SLOT7 -o slot7.o
-;ld65 -t none slot1.o slot2.o slot3.o slot4.o slot5.o slot6.o slot7.o -o Firmware.bin
+;ld65 -t none warning.o slot1.o slot2.o slot3.o slot4.o slot5.o slot6.o slot7.o -o Firmware.bin
 
 ;Determine which slot we want by command-line define
 	.ifdef SLOT1
