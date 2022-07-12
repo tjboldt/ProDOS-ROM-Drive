678e68b1c4781f0e873b6e9fae6996dad0dcaf90
Terence Boldt <terence@boldt.ca>
2022-07-12 18:32:24 -0400

    Update driver to not need JSR $FF58 on entry
    Save and restore zero page values

--- Firmware/Firmware.asm
+++ Firmware/Firmware.asm
@@ -2,12 +2,6 @@
   writeLatchHigh = $C081
   writeLatchLow = $C080
 
-;temp variables becasue 6502 only has 3 registers
-	highLatch = $F8
-	lowLatch = $F9
-	tempY = $FE
-	blockHalfCounter = $FF   
-
 ;ProDOS defines
   command = $42   ;ProDOS command
   unit  = $43  ;7=drive 6-4=slot 3-0=not used
@@ -19,158 +13,174 @@
   nodev = $28  ;no device connected
   wperr = $2B  ;write protect error
 
-;for relocatable code, address to jump to instead of JSR absolute + RTS
-	jumpAddressLo = $FA
-	jumpAddressHi = $FB 
-	ioAddressLo   = $FC
-	ioAddressHi   = $FD
-	knownRts   = $FF58
+;for relocatable code
+  ioAddressLo   = $FA
+  ioAddressHi   = $FB
+  tempY = $FC
+  blockHalfCounter = $FD
+  lowLatch = $FE
+  highLatch = $FF
+  knownRts   = $FF58
 
-	.org  $C700
+  .org  $C700
   ;code is relocatable
   ; but set to $c700 for
   ; readability
 
 ;ID bytes for booting and drive detection
-	cpx  #$20    ;ID bytes for ProDOS and the
-	ldy  #$00    ; Apple Autostart ROM
-	cpx  #$03    ;
-	cpx  #$3C    ;this one for older II's
+  cpx  #$20    ;ID bytes for ProDOS and the
+  ldy  #$00    ; Apple Autostart ROM
+  cpx  #$03    ;
+  cpx  #$3C    ;this one for older II's
 
 ;zero out block numbers and buffer address
-	sty	 buflo
-	sty  blklo
-	sty  blkhi
-	iny				;set command = 1 for read block
-	sty  command 
-	sty  jumpAddressLo ;$01 of $0801 where boot code starts
-	jsr  knownRts ;jump to known RTS to get our address from the stack
-	tsx
+  sty	 buflo
+  sty  blklo
+  sty  blkhi
+  iny				;set command = 1 for read block
+  sty  command 
+  jsr  knownRts ;jump to known RTS to get our address from the stack
+  tsx
   lda  $0100,x ;this for example would be $C7 in slot 7
   sta  bufhi   ;keep the slot here
   asl   
   asl   
   asl   
   asl   
-  tax			
+  sta unit			
 
 ;display copyright message
   ldy  #<text
 drawtxt:
   lda  (buflo),y
   beq  boot
-	sta  $07D0-<text,y ;put text on last line
-	iny
-	bne  drawtxt
+  sta  $07D0-<text,y ;put text on last line
+  iny
+  bne  drawtxt
 
 ;load block 0000 at $0800
 boot:
-	lda  #$08
-	sta  bufhi
-	sta  jumpAddressHi
-	stx  unit
-	bne  start
+  lda  #$08 ;push $0800 onto the stack so an RTS will run at $0801
+  sta  bufhi
+  pha
+  lda #$00
+  pha
 ;This is the ProDOS entry point for this card
 entry:
-	lda  #<knownRts
-	sta  jumpAddressLo
-	lda  #>knownRts
-	sta  jumpAddressHi
-start:
-	lda  #$C0
-	sta  ioAddressHi
-	jsr  knownRts
-	tsx
-	lda  $0100,x
-	asl  a
-	asl  a
-	asl  a
-	asl  a
-	tax
-	cpx  unit	 ;make sure same as ProDOS
-	beq  docmd ;yep, do command
-	sec				 ;nope, set device not connected
-	lda  #nodev
-	rts        ;go back to ProDOS
+  ldx  unit  ;make sure it's drive 1
+  bpl  docmd ;yep, do command
+  sec				 ;nope, set device not connected
+  lda  #nodev
+  rts        ;go back to ProDOS
 
 docmd:
-	lda  command
-	beq  getstat ;command 0 is GetStatus
-	cmp  #$01
-	beq  readblk ;command 1 is ReadBlock
-	sec          ;Format/Write not permitted
-	lda  #wperr  ;write protect error
-	rts          ;go back to ProDOS
+  lda  command
+  beq  getstat ;command 0 is GetStatus
+  cmp  #$01
+  beq  readblk ;command 1 is ReadBlock
+  sec          ;Format/Write not permitted
+  lda  #wperr  ;write protect error
+  rts          ;go back to ProDOS
 
 getstat:  
-	clc       ;send back status
-	lda  #$00 ;good status
-	ldx  #$00 ;1024 blocks
-	ldy  #$04 ;
-	rts
+  clc       ;send back status
+  lda  #$00 ;good status
+  ldx  #$00 ;1024 blocks
+  ldy  #$04 ;
+  rts
 
 readblk:
-	lda  blkhi	;get hi block
-	asl  a    ;shift up to top 3 bits
-	asl  a    ;since that's all the high
-	asl  a    ;blocks we can handle
-	asl  a    ;
-	asl  a    ;
-	sta  highLatch   ;save it in scratch ram 0
-			;so we can stuff it in the
-			;high latch later
-	lda  blklo   ;get low block
-	lsr  a    ;shift so we get the top 5
-	lsr  a    ;bits - this also goes in
-	lsr  a    ;the high latch
-	ora  highLatch ;add it to those top 3 bits
+  lda ioAddressLo
+  pha
+  lda ioAddressHi
+  pha
+  lda tempY
+  pha
+  lda blockHalfCounter
+  pha
+  lda lowLatch
+  pha
+  lda highLatch
+  pha
+  
+  lda  blkhi	;get hi block
+  asl  a    ;shift up to top 3 bits
+  asl  a    ;since that's all the high
+  asl  a    ;blocks we can handle
+  asl  a    ;
+  asl  a    ;
+  sta  highLatch 
+  lda  blklo   ;get low block
+  lsr  a    ;shift so we get the top 5
+  lsr  a    ;bits - this also goes in
+  lsr  a    ;the high latch
+  ora  highLatch ;add it to those top 3 bits
 	sta  highLatch ;save it back in scratch ram
-	lda  blklo   ;get low block
-	asl  a    ;shift it to top 3 bits
-	asl  a    ;
-	asl  a    ;
-	asl  a    ;
-	asl  a    ;
-	sta  lowLatch
-	lda  #$02
-	sta  blockHalfCounter
+  sta  writeLatchHigh,x	;set high latch for card
+  lda  blklo   ;get low block
+  asl  a    ;shift it to top 3 bits
+  asl  a    ;
+  asl  a    ;
+  asl  a    ;
+  asl  a    ;
+  sta lowLatch
+  lda  #$02
+  sta  blockHalfCounter
+  lda #$C0
+  sta ioAddressHi
 
 ;This gets 256 bytes from the ROM card
 
 read256:
-	ldy  #$00
-	lda  highLatch	;get high latch value
-	sta  writeLatchHigh,x	;set high latch for card
+  ldy  #$00
 loop256:
-	lda  lowLatch
-	sta  writeLatchLow,x
-	txa
-	ora  #$80
-	sta  ioAddressLo
+  lda  lowLatch
+  sta  writeLatchLow,x
+  txa
+  ora  #$80
+  sta  ioAddressLo
+
 loop16:
-	sty  tempY
-	ldy  #$00
-	lda  (ioAddressLo),y
-	ldy  tempY
-	sta  (buflo),y
-	iny
-	inc  ioAddressLo
-	lda  ioAddressLo
-	and  #$0F
-	bne  loop16
-	inc  lowLatch
-	cpy  #$00
-	bne  loop256
-	dec  blockHalfCounter
-	bne  readnext256
-	dec  bufhi
-	clc       ;clear error code for success
-	lda  #$00
-	jmp  (jumpAddressLo)
+  sty  tempY
+  ldy  #$00
+  lda  (ioAddressLo),y
+  ldy  tempY
+  sta  (buflo),y
+  iny
+  inc  ioAddressLo
+  lda  ioAddressLo
+  and  #$0F
+  bne  loop16
+
+continue256:
+  inc  lowLatch
+  cpy  #$00
+  bne  loop256
+  dec  blockHalfCounter
+  bne  readnext256
+
+finish:
+  pla
+  sta highLatch
+  pla
+  sta lowLatch
+  pla
+  sta blockHalfCounter
+  pla
+  sta tempY
+  pla
+  sta ioAddressHi
+  pla
+  sta ioAddressLo
+
+  dec  bufhi
+  clc       ;clear error code for success
+  lda  #$00
+  rts
 
 readnext256:  
-	inc  bufhi
-  clc
+  inc  bufhi   ; set buffer to receive next 256 bytes
+  clc          ; effectively a branch always
   bcc  read256
 
 ;macro for string with high-bit set
